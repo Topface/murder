5bd9ad6e3df4a0a284f38b4ea74b5e73b31eb923
fufar <kibko.ilya@gmail.com>
2023-08-14 20:18:04 +0700

    a

--- dist/BitTornado/bitfield.py
+++ dist/BitTornado/bitfield.py
@@ -1,163 +1,121 @@
-from functools import reduce
-# Written by Bram Cohen, Uoti Urpala, and John Hoffman
-# see LICENSE.txt for license information
-
-try:
-    True
-except:
-    True = 1
-    False = 0
-    bool = lambda x: not not x
-
-try:
-    sum([1])
-    negsum = lambda a: len(a)-sum(a)
-except:
-    negsum = lambda a: reduce(lambda x,y: x+(not y), a, 0)
-    
-def _int_to_booleans(x):
-    r = []
-    for i in range(8):
-        r.append(bool(x & 0x80))
-        x <<= 1
-    return tuple(r)
-
-lookup_table = []
-reverse_lookup_table = {}
-for i in range(256):
-    x = _int_to_booleans(i)
-    lookup_table.append(x)
-    reverse_lookup_table[x] = chr(i)
+"""Manipulable boolean list structure and related functions"""
 
 
-class Bitfield:
-    def __init__(self, length = None, bitstring = None, copyfrom = None):
+def _int_to_booleans(integer):
+    """Produce a tuple of booleans corresponding to 8 least significant bits
+    in an integer
+    """
+    return tuple(bool((integer << nbits) & 0x80) for nbits in xrange(8))
+
+CHARBITMAP = [_int_to_booleans(_chr) for _chr in xrange(256)]
+
+BITCHARMAP = dict((_bits, chr(_chr)) for _chr, _bits in enumerate(CHARBITMAP))
+
+
+class Bitfield(list):
+    """Allow a sequence of booleans to be used as an indexable bitfield"""
+    def __init__(self, length=None, bitstring=None, copyfrom=None, val=False):
         if copyfrom is not None:
-            self.length = copyfrom.length
-            self.array = copyfrom.array[:]
+            super(Bitfield, self).__init__(copyfrom)
             self.numfalse = copyfrom.numfalse
             return
         if length is None:
-            raise ValueError("length must be provided unless copying from another array")
-        self.length = length
+            raise ValueError('length must be provided unless copying from '
+                             'another array')
         if bitstring is not None:
             extra = len(bitstring) * 8 - length
             if extra < 0 or extra >= 8:
                 raise ValueError
-            t = lookup_table
-            r = []
-            for c in bitstring:
-                r.extend(t[ord(c)])
+
+            bits = [bit for char in bitstring for bit in CHARBITMAP[ord(char)]]
             if extra > 0:
-                if r[-extra:] != [0] * extra:
+                if bits[-extra:] != [False] * extra:
                     raise ValueError
-                del r[-extra:]
-            self.array = r
-            self.numfalse = negsum(r)
+                del bits[-extra:]
+            self.numfalse = len(bits) - sum(bits)
         else:
-            self.array = [False] * length
-            self.numfalse = length
+            bits = [val] * length
+            self.numfalse = 0 if val else length
+
+        super(Bitfield, self).__init__(bits)
 
     def __setitem__(self, index, val):
         val = bool(val)
-        self.numfalse += self.array[index]-val
-        self.array[index] = val
+        self.numfalse += self[index] - val
+        super(Bitfield, self).__setitem__(index, val)
 
-    def __getitem__(self, index):
-        return self.array[index]
+    def __repr__(self):
+        return "<Bitfield ({})>".format(','.join(str(int(i)) for i in self))
 
-    def __len__(self):
-        return self.length
+    def __str__(self):
+        """Produce a bytestring corresponding to the current bitfield"""
+        bits = self + [False] * (-len(self) % 8)
+        return ''.join(BITCHARMAP[tuple(bits[x:x + 8])]
+                       for x in xrange(0, len(bits), 8))
 
-    def tostring(self):
-        booleans = self.array
-        t = reverse_lookup_table
-        s = len(booleans) % 8
-        r = [ t[tuple(booleans[x:x+8])] for x in range(0, len(booleans)-s, 8) ]
-        if s:
-            r += t[tuple(booleans[-s:] + ([0] * (8-s)))]
-        return ''.join(r)
-
+    @property
     def complete(self):
+        """True if all booleans are True"""
         return not self.numfalse
 
 
+class TrueBitfield(object):     # pylint: disable=R0903
+    """A trivial structure that acts like an infinitely long field of
+    True bits"""
+    complete = True
+
+    def __getitem__(self, index):
+        return True
+
+
+def _test_exception(exc, func, *data):
+    """Validate that func(data) raises exc"""
+    try:
+        func(*data)
+    except exc:
+        return True
+    except:
+        pass
+    return False
+
+
 def test_bitfield():
-    try:
-        x = Bitfield(7, 'ab')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(7, 'ab')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(9, 'abc')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(0, 'a')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(1, '')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(7, '')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(8, '')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(9, 'a')
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(7, chr(1))
-        assert False
-    except ValueError:
-        pass
-    try:
-        x = Bitfield(9, chr(0) + chr(0x40))
-        assert False
-    except ValueError:
-        pass
-    assert Bitfield(0, '').tostring() == ''
-    assert Bitfield(1, chr(0x80)).tostring() == chr(0x80)
-    assert Bitfield(7, chr(0x02)).tostring() == chr(0x02)
-    assert Bitfield(8, chr(0xFF)).tostring() == chr(0xFF)
-    assert Bitfield(9, chr(0) + chr(0x80)).tostring() == chr(0) + chr(0x80)
-    x = Bitfield(1)
-    assert x.numfalse == 1
-    x[0] = 1
-    assert x.numfalse == 0
-    x[0] = 1
-    assert x.numfalse == 0
-    assert x.tostring() == chr(0x80)
-    x = Bitfield(7)
-    assert len(x) == 7
-    x[6] = 1
-    assert x.numfalse == 6
-    assert x.tostring() == chr(0x02)
-    x = Bitfield(8)
-    x[7] = 1
-    assert x.tostring() == chr(1)
-    x = Bitfield(9)
-    x[8] = 1
-    assert x.numfalse == 8
-    assert x.tostring() == chr(0) + chr(0x80)
-    x = Bitfield(8, chr(0xC4))
-    assert len(x) == 8
-    assert x.numfalse == 5
-    assert x.tostring() == chr(0xC4)
+    """Unit test Bitfield"""
+    assert _test_exception(ValueError, Bitfield, 7, 'ab')
+    assert _test_exception(ValueError, Bitfield, 7, 'ab')
+    assert _test_exception(ValueError, Bitfield, 9, 'abc')
+    assert _test_exception(ValueError, Bitfield, 0, 'a')
+    assert _test_exception(ValueError, Bitfield, 1, '')
+    assert _test_exception(ValueError, Bitfield, 7, '')
+    assert _test_exception(ValueError, Bitfield, 8, '')
+    assert _test_exception(ValueError, Bitfield, 9, 'a')
+    assert _test_exception(ValueError, Bitfield, 7, chr(1))
+    assert _test_exception(ValueError, Bitfield, 9, chr(0) + chr(0x40))
+    assert str(Bitfield(0, '')) == ''
+    assert str(Bitfield(1, chr(0x80))) == chr(0x80)
+    assert str(Bitfield(7, chr(0x02))) == chr(0x02)
+    assert str(Bitfield(8, chr(0xFF))) == chr(0xFF)
+    assert str(Bitfield(9, chr(0) + chr(0x80))) == chr(0) + chr(0x80)
+    testx = Bitfield(1)
+    assert testx.numfalse == 1
+    testx[0] = 1
+    assert testx.numfalse == 0
+    testx[0] = 1
+    assert testx.numfalse == 0
+    assert str(testx) == chr(0x80)
+    testx = Bitfield(7)
+    assert len(testx) == 7
+    testx[6] = 1
+    assert testx.numfalse == 6
+    assert str(testx) == chr(0x02)
+    testx = Bitfield(8)
+    testx[7] = 1
+    assert str(testx) == chr(1)
+    testx = Bitfield(9)
+    testx[8] = 1
+    assert testx.numfalse == 8
+    assert str(testx) == chr(0) + chr(0x80)
+    testx = Bitfield(8, chr(0xC4))
+    assert len(testx) == 8
+    assert testx.numfalse == 5
+    assert str(testx) == chr(0xC4)
--- dist/BitTornado/clock.py
+++ dist/BitTornado/clock.py
@@ -1,27 +1,41 @@
-# Written by John Hoffman
-# see LICENSE.txt for license information
+"""Provide a non-decreasing clock() function.
 
-from time import *
+In Windows, time.clock() provides number of seconds from first call, so use
+that.
+
+In Unix, time.clock() is CPU time, and time.time() reports system time, which
+may not be non-decreasing."""
+
+import time
 import sys
 
 _MAXFORWARD = 100
 _FUDGE = 1
 
-class RelativeTime:
+
+class RelativeTime(object):     # pylint: disable=R0903
+    """Non-decreasing time implementation for Unix"""
     def __init__(self):
-        self.time = time()
+        self.time = time.time()
         self.offset = 0
 
-    def get_time(self):        
-        t = time() + self.offset
-        if t < self.time or t > self.time + _MAXFORWARD:
+    def get_time(self):
+        """Calculate a non-decreasing time representation"""
+        systemtime = time.time()
+
+        now = systemtime + self.offset
+
+        if self.time < now < self.time + _MAXFORWARD:
+            self.time = now
+        else:
+            # If time jump is outside acceptable bounds, move ahead one second
+            # and note the offset
             self.time += _FUDGE
-            self.offset += self.time - t
-            return self.time
-        self.time = t
-        return t
+            self.offset = self.time - systemtime
+
+        return self.time
 
 if sys.platform != 'win32':
-    _RTIME = RelativeTime()
-    def clock():
-        return _RTIME.get_time()
+    clock = RelativeTime().get_time     # pylint: disable=C0103
+else:
+    from time import clock
